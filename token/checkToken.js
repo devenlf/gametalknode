fe0d37849e192fea0c92f7123e96243c46af442b
lifei <lifei@31huiyi.com>
2018-08-24 10:55:25 +0800

    保存

--- token/checkToken.js
+++ token/checkToken.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 module.exports = async (ctx, next) => {
   const authorization = ctx.request.header['token'];
+  console.log(authorization)
   jwt.verify(authorization, 'sinner77', function (err, decoded) {
     if (err) {
       ctx.body = {
