9fb7e0fe78246ca9bc97d0fa23b0aaa829225b81
lifei <lifei@31huiyi.com>
2018-09-03 19:26:35 +0800

    注册逻辑

--- routes/users.js
+++ routes/users.js
@@ -5,40 +5,55 @@ const User = require('../db/userInfo')
 router.prefix('/users')
 
 router.post('/login', function (ctx, next) {
-  if(!ctx.request.body.username||!ctx.request.body.password){
-    ctx.body={
-      message:'用户名或者密码不得为空',
-      state:0
+  if (!ctx.request.body.username || !ctx.request.body.password) {
+    ctx.body = {
+      message: '用户名或者密码不得为空',
+      state: 0
     }
     return
-  }else{
-    if(ctx.request.body.username==='lifei'&&ctx.request.body.password==='123456'){
+  } else {
+    if (ctx.request.body.username === 'lifei' && ctx.request.body.password === '123456') {
       let token = createToken('1')
-      ctx.body={
-        message:'登录成功',
+      ctx.body = {
+        message: '登录成功',
         name: "李飞",
-        userId:1,
+        userId: 1,
         token: token,
-        state:2
+        state: 2
       }
-    }else{
-      ctx.body={
-        message:'用户名密码不正确',
-        state:1
+    } else {
+      ctx.body = {
+        message: '用户名密码不正确',
+        state: 1
       }
     }
   }
 })
 
-router.post('/register',async function(ctx,next){
-  console.log(ctx.request.body)
-  await addUserInfo(ctx.request.body)
+router.post('/register', async function (ctx, next) {
+  let requestData = ctx.request.body
+  var wherestr = { 'username': requestData.username };
+  let userResult = await User.find(wherestr)
+  if (userResult.length > 0) {
+    ctx.body = {
+      scucces: false,
+      message: "用户名存在"
+    }
+    return
+  }
+  await addUserInfo(requestData)
+  ctx.body = {
+    scucces: true,
+    message: "注册成功"
+  }
+  return
 })
 
 router.post('/bar', function (ctx, next) {
-  
+
 })
 
+//插入数据
 function addUserInfo(data) {
   var user = new User({
     nackname: data.nackname,
@@ -48,13 +63,15 @@ function addUserInfo(data) {
     phone: data.phone                     //最近登录时间
   });
   user.save(function (err, res) {
-      if (err) {
-          console.log("Error:" + err);
-      }
-      else {
-          console.log("Res:" + res);
-      }
+    if (err) {
+      console.log("Error:" + err);
+    }
+    else {
+      console.log("Res:" + res);
+    }
   });
 }
+
+
 
 module.exports = router
