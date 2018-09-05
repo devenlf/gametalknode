88b5e8f8e221ead45640ef1736dc9b75296f7eca
lifei <lifei@31huiyi.com>
2018-09-05 18:13:22 +0800

    保存

--- routes/game.js
+++ routes/game.js
@@ -1,20 +1,22 @@
 const router = require('koa-router')()
-var filesActive = require('../modules/files')
 const checkToken = require('../token/checkToken')
 
 
 router.prefix('/game')
-router.post('/list', async (ctx, next) => {
+router.post('/list', async function (ctx, next) {
   let token = ctx.request.header['token']
-  let user_Id =await checkToken(token).user_id;
-  return
-
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
+  try {
+    await checkToken(token);
+    ctx.body={
+      status:1
+    }
+  }catch(err){
+    ctx.body={
+      status:0
+    }
   }
+  return
 })
+
 
 module.exports = router
--- token/checkToken.js
+++ token/checkToken.js
@@ -1,14 +1,5 @@
 const jwt = require('jsonwebtoken');
 module.exports = (token) => {
-  jwt.verify(token, 'sinner77', function (err, decoded) {
-    if (err) {
-      ctx.body = {
-        state: 0,
-        name: 'JsonWebTokenError',
-        message: 'jwt malformed'
-      }
-      return
-    }
-  })
-  return decoded
-}
+  return jwt.verify(token, 'sinner77',function(err, decoded){
+    console.log(decoded)
+  })}
